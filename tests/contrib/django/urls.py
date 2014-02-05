bfe639394a05e5e2396b7f16416f50343dacb92d
Piotr Skamruk <piotr.skamruk@gmail.com>
2014-02-05 15:58:22 +0100

    Refactor django tests.

--- tests/contrib/django/urls.py
+++ tests/contrib/django/urls.py
@@ -20,13 +20,13 @@ def handler500(request):
     return HttpResponse('', status=500)
 
 
-urlpatterns = patterns('',
-    url(r'^no-error$', 'tests.contrib.django.views.no_error', name='sentry-no-error'),
-    url(r'^fake-login$', 'tests.contrib.django.views.fake_login', name='sentry-fake-login'),
-    url(r'^trigger-500$', 'tests.contrib.django.views.raise_exc', name='sentry-raise-exc'),
-    url(r'^trigger-500-ioerror$', 'tests.contrib.django.views.raise_ioerror', name='sentry-raise-ioerror'),
-    url(r'^trigger-500-decorated$', 'tests.contrib.django.views.decorated_raise_exc', name='sentry-raise-exc-decor'),
-    url(r'^trigger-500-django$', 'tests.contrib.django.views.django_exc', name='sentry-django-exc'),
-    url(r'^trigger-500-template$', 'tests.contrib.django.views.template_exc', name='sentry-template-exc'),
-    url(r'^trigger-500-log-request$', 'tests.contrib.django.views.logging_request_exc', name='sentry-log-request-exc'),
+urlpatterns = patterns('tests.contrib.django.views',
+    url(r'^no-error$', 'no_error', name='sentry-no-error'),
+    url(r'^fake-login$', 'fake_login', name='sentry-fake-login'),
+    url(r'^trigger-500$', 'raise_exc', name='sentry-raise-exc'),
+    url(r'^trigger-500-ioerror$', 'raise_ioerror', name='sentry-raise-ioerror'),
+    url(r'^trigger-500-decorated$', 'decorated_raise_exc', name='sentry-raise-exc-decor'),
+    url(r'^trigger-500-django$', 'django_exc', name='sentry-django-exc'),
+    url(r'^trigger-500-template$', 'template_exc', name='sentry-template-exc'),
+    url(r'^trigger-500-log-request$', 'logging_request_exc', name='sentry-log-request-exc'),
 )
